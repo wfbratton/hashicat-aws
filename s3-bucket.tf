0537f54d2cbbcfb565fa7b068400b24e4b56f325
Will Bratton <william.bratton@lmco.com>
2022-06-23 20:58:47 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,11 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+  bucket_prefix = "will-bratton"
+  versioning = {
+    enabled = true
+  }
+
+}
